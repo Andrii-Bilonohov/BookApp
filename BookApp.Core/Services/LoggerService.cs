1bd341eeefa9a353448e9d55d6bd2f13d88483ff
BXlon_ub54@student.itstep.org <BXlon_ub54@student.itstep.org>
2025-06-24 18:36:15 +0300

    Refactoring code

--- BookApp.Core/Services/LoggerService.cs
+++ BookApp.Core/Services/LoggerService.cs
@@ -1,48 +1,32 @@
 ﻿using BookApp.Core.Interfaces;
-using System;
-using System.IO;
-using System.Threading;
 
 namespace BookApp.Core.Services
 {
     public class LoggerService : ILoggerService
     {
-        private readonly string _logDirectory;
+        private readonly string _logDirectory = "C:/WebApi/BookApp/BookApp.Core/Logs/";
 
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
-
-
-        public LoggerService()
-        {
-            _logDirectory = Path.Combine(AppContext.BaseDirectory, "Logs");
-            if (!Directory.Exists(_logDirectory))
-            {
-                Directory.CreateDirectory(_logDirectory);
-            }
-        }
 
 
         public void LogInfo(string message)
         {
             var logMessage = $"INFO: {message} ({DateTime.Now:yyyy-MM-dd HH:mm:ss.fff})";
-            Console.WriteLine(logMessage);
-            WriteLogToFile("info.log", logMessage);
+            WriteLogToFile("info.txt", logMessage);
         }
 
 
         public void LogWarning(string message)
         {
             var logMessage = $"WARNING: {message} ({DateTime.Now:yyyy-MM-dd HH:mm:ss.fff})";
-            Console.WriteLine(logMessage);
-            WriteLogToFile("warning.log", logMessage);
+            WriteLogToFile("warning.txt", logMessage);
         }
 
 
         public void LogError(string message, Exception ex)
         {
             var logMessage = $"ERROR: {message}. Exception: {ex.Message} ({DateTime.Now:yyyy-MM-dd HH:mm:ss.fff})";
-            Console.WriteLine(logMessage);
-            WriteLogToFile("error.log", logMessage);
+            WriteLogToFile("error.txt", logMessage);
         }
 
 
